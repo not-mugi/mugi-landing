adef9dc8ecd37131c6f5f5fd8018403d0668f0b6
Zwe Sithu <withzues@gmail.com>
2026-06-15 11:21:10 +0630

    chore: add assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindCss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite"
 
 export default defineConfig({
-  plugins: [tanstackStart(), tailwindCss(), viteReact()],
+  plugins: [tanstackStart(), nitro(), tailwindCss(), viteReact()],
   resolve: {
     tsconfigPaths: true,
   },
